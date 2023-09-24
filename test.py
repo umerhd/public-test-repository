dc3235f69ed0f9c052bb7d50ae5a656bb8a779f7
Umer Haider <48306968+umerhd@users.noreply.github.com>
2023-09-24 02:41:04 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, x):
+        """Find the representative of the set that x belongs to."""
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        """Union the sets that x and y belong to."""
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            # Union by rank to ensure that the tree remains flat.
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            else:
+                self.parent[rootX] = rootY
+                if self.rank[rootX] == self.rank[rootY]:
+                    self.rank[rootY] += 1
